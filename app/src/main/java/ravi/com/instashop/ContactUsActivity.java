92ceae9e69fb16c03653da1eabeda5066161c9fa
nikpatel <tp007143007@gmail.com>
2018-03-07 14:43:18 +0530

    Ravi Contactus Done  7-3-18

--- app/src/main/java/ravi/com/instashop/ContactUsActivity.java
+++ app/src/main/java/ravi/com/instashop/ContactUsActivity.java
@@ -2,6 +2,9 @@ package ravi.com.instashop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ContactUsActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class ContactUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.contact_us_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.nav_action_send) {
+            Toast.makeText(this, "Send Success", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
